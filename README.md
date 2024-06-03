a0a6e73049c6d8209898b744ad26f2bb68dd3c60
Akanit Kwangkaew <akanitk84@gmail.com>
2024-06-03 21:55:59 +0700

    update

--- README.md
+++ README.md
@@ -10,10 +10,11 @@ https://youtu.be/eltKL8kC160
 
 - Required packages
 > pip install SQLAlchemy
+
 > pip install fastapi
 
 - Create folder 'src'
 > python3 -m venv venv
 
 - To start server : 
-> src % uvicorn main:app --reload
+> uvicorn main:app --reload
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+
+from typing import List
 import fastapi as _fastapi
 import sqlalchemy.orm as _orm
 import services as _services, schemas as _schemas
@@ -13,3 +15,12 @@ def create_user(user:_schemas.UserCreate,
     if db_user:
         raise _fastapi.HTTPException(status_code=400, detail="Opps! the email is in use.")
     return _services.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=List[_schemas.User])
+def read_users(
+    skip: int=0, 
+    limit: int=10, 
+    db: _orm.Session=_fastapi.Depends(_services.get_db),
+):
+    
+    pass
